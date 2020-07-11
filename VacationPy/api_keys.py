45023762e63c9bc5732578ed1421bf280d73d395
hugo6garcia <60806192+hugo6garcia@users.noreply.github.com>
2020-07-10 19:00:08 -0700

    Update api_keys.py

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "e6faf1ad8108ea7b5d60fbf029b80e7c"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyA9kWoGnuBph4cajygqhdQdXlqbxzr2nfM"
+g_key = ""
